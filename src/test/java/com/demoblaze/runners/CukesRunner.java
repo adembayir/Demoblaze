6981a728ffb114f069856c063dbe8d8b6864e061
Adem BAYIR <adembyr@hotmail.com>
2022-06-24 20:08:39 +0300

    step definitions is created

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html=target/cucumber-report.html"
+                "html:target/cucumber-report.html"
         },
         features = "src/test/resources/features",
         glue = "com/demoblaze/step_definitions",
--- src/test/java/com/demoblaze/step_definitions/TestStepDefs.java
+++ src/test/java/com/demoblaze/step_definitions/TestStepDefs.java
@@ -1,4 +1,20 @@
 package com.demoblaze.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
 public class TestStepDefs {
+
+    @Given("buyer is on homepage of demoblaze.com")
+    public void buyer_is_on_homepage_of_demoblaze_com() {
+
+    }
+    @When("buyer adds from {string} category {string} product to cart")
+    public void buyer_adds_from_category_product_to_cart(String category, String product) {
+
+    }
+    @When("buyer clicks on {string} link")
+    public void buyer_clicks_on_link(String link) {
+
+    }
 }
